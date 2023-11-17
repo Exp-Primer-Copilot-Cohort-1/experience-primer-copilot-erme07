51418c88a25bc293e0226ed2e939229d9cfb42af
Erik Medina <46094137+erme07@users.noreply.github.com>
2023-11-17 21:22:32 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,1 +1,18 @@
-// Create web server with express
+// Create web server by using Express
+// Date: 2021/05/24
+// Creator: Nhat Hoang
+
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+router.get('/', commentController.getComments);
+
+router.post('/', authMiddleware, commentController.createComment);
+
+router.put('/', authMiddleware, commentController.updateComment);
+
+router.delete('/', authMiddleware, commentController.deleteComment);
+
+module.exports = router;
